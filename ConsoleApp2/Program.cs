8dd0fb197756435a90e93bf51542d4d05e191f49
STUDENTAidenRapoza <STUDENTAidenRapoza@TECH02>
2023-10-27 08:32:48 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Random instult generator, press a key");
+string[] insults = { "You suck", "You smell", "Your mom can't fit inside an elevator","You're so skinny you look like Titus (insult from Titus)" };
+Random rand = new Random();
+int num = 0;
+while (1 < 2)
+{
+    Console.ReadKey();
+    Console.Clear();
+    num = rand.Next(0,4);
+    Console.WriteLine(insults[num]);
+}
